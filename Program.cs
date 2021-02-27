3068fcddda4f2bf01d3f8961d677f0d8e9bd425a
Rubyblex <rubyblex@rubygenix.com>
2021-02-27 13:16:33 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Terracotta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
